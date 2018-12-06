a57889a434f81702d865154b86b7253037d50ead
ZSUU <zhongshuyx@sina.com>
2018-12-06 11:01:30 +0800

    update

--- test/setup.ts
+++
@@ -1,8 +0,0 @@
-import { JSDOM } from 'jsdom'
-
-let { window } = new JSDOM('<!doctype html><html><body></body></html>')
-
-global['window'] = window
-global['document'] = window.document
-global['navigator'] = { userAgent: 'node.js' }
-global['HTMLElement'] = window.HTMLElement
